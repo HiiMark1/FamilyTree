0430532d937b738e83f46382981269879cbf7cc9
Dungeon Master <65589740+HiiMark1@users.noreply.github.com>
2023-10-22 00:19:07 +0300

    empty project with fixed bottom navigation

--- androidApp/src/main/java/com/example/myapplication/android/MainFragment.kt
+++ androidApp/src/main/java/com/example/myapplication/android/MainFragment.kt
@@ -1,6 +1,20 @@
 package com.example.myapplication.android
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.myapplication.android.databinding.FragmentMainBinding
 
 class MainFragment: Fragment(R.layout.fragment_main) {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+
+        binding.button.setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_mainFragment_to_profileFragment)
+        }
+    }
 }
--- androidApp/src/main/java/com/example/myapplication/android/ProfileFragment.kt
+++ androidApp/src/main/java/com/example/myapplication/android/ProfileFragment.kt
@@ -1,6 +1,20 @@
 package com.example.myapplication.android
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.myapplication.android.databinding.FragmentProfileBinding
 
 class ProfileFragment: Fragment(R.layout.fragment_profile) {
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentProfileBinding.bind(view)
+
+        binding.textView4.setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_profileFragment_to_mainFragment)
+        }
+    }
 }
